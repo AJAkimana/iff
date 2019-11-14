0088a6db3c503407cca4e82dfd864c8a50645c0d
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-11-14 13:16:07 +0200

    Create indirecting users

--- php-includes/treeUtil.php
+++ php-includes/treeUtil.php
@@ -14,17 +14,27 @@
             $this->rightCount = $rightCount;
         }
 
-        public function matchingUsers()
+        public function matchUsers()
         {
             $match = 0;
             if ($this->leftCount < $this->rightCount) {
                 $match = $this->leftCount;
             } elseif ($this->leftCount > $this->rightCount) {
                 $match = $this->leftCount;
-            } else {
+            } elseif ($this->leftCount == $this->rightCount) {
                 $match = $this->leftCount = $this->rightCount;
             }
 
             return $match + 1;
+        }
+
+        public function indirectUsers()
+        {
+            $indirect = 0;
+            if ($this->leftCount > 2 && $this->rightCount > 2) {
+                $indirect = 1;
+            }
+
+            return $indirect;
         }
     }
