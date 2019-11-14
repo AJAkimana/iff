a9b6ea5be95dbf36ca7ca417f3188dfbb9086a44
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-11-14 13:11:21 +0200

    Create matching users

--- php-includes/treeUtil.php
+++ php-includes/treeUtil.php
@@ -1,3 +1,30 @@
 <?php
 
-  include 'connect.php';
+    class AmountCalculator
+    {
+        private $conn;
+        private $leftCount;
+        private $rightCount;
+
+        public function __construct($leftCount, $rightCount)
+        {
+            require_once 'connect.php';
+            $this->conn = $con;
+            $this->leftCount = $leftCount;
+            $this->rightCount = $rightCount;
+        }
+
+        public function matchingUsers()
+        {
+            $match = 0;
+            if ($this->leftCount < $this->rightCount) {
+                $match = $this->leftCount;
+            } elseif ($this->leftCount > $this->rightCount) {
+                $match = $this->leftCount;
+            } else {
+                $match = $this->leftCount = $this->rightCount;
+            }
+
+            return $match + 1;
+        }
+    }
